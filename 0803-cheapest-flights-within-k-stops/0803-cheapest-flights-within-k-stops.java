07091a67cefb1a6453c46f94d47cd200f5db1967
Saumya Singh <43070615+Saumya1915@users.noreply.github.com>
2024-08-17 17:16:04 +0530

    Time: 12 ms (17.81%), Space: 44.2 MB (56.95%) - LeetHub

--- 0803-cheapest-flights-within-k-stops/0803-cheapest-flights-within-k-stops.java
+++ 0803-cheapest-flights-within-k-stops/0803-cheapest-flights-within-k-stops.java
@@ -26,23 +26,28 @@ class Solution {
             
         }
         
-        PriorityQueue<Pair> pq = new PriorityQueue<>((a, b) -> a.wt - b.wt);
-        pq.add(new Pair(src, 0, -1));
-        int[] stop = new int[n];
-        Arrays.fill(stop, Integer.MAX_VALUE);
-        
-        while(pq.size() > 0){
-            Pair p = pq.remove();
-            if(p.dist > k || stop[p.v] < p.dist)continue;
-            
-            stop[p.v] = p.dist;
-            if(p.v == dst)return p.wt;
+        PriorityQueue<Pair> pq=new PriorityQueue<>((a,b)->a.wt-b.wt);
 
-            for(Pair nbr : graph.get(p.v)){
-                pq.add(new Pair(nbr.v, p.wt + nbr.wt, p.dist + 1));
-                
+        pq.add(new Pair(src,0,-1));
+         int[] stop = new int[n];
+        Arrays.fill(stop, Integer.MAX_VALUE);
+
+        while(!pq.isEmpty())
+        {
+            Pair p=pq.remove();
+
+            if(p.v==dst)
+            {
+                return p.wt;
             }
-            
+
+            for(Pair it:graph.get(p.v)){
+
+                if(p.dist+1<=k && stop[it.v]> p.dist){
+                    pq.add(new Pair(it.v,p.wt+it.wt,p.dist+1));
+                    stop[p.v] = p.dist;
+                }
+            }
         }
         return -1;
     }
