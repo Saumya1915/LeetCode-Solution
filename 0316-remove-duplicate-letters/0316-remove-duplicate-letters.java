4145f83e2ce0b0bb9a6386164f5e3d6031577c8b
Saumya Singh <43070615+Saumya1915@users.noreply.github.com>
2024-10-15 11:36:25 +0530

    Time: 19 ms (5.71%), Space: 42 MB (72.14%) - LeetHub

--- 0316-remove-duplicate-letters/0316-remove-duplicate-letters.java
+++ 0316-remove-duplicate-letters/0316-remove-duplicate-letters.java
@@ -1,26 +1,34 @@
 class Solution {
     public String removeDuplicateLetters(String s) {
-        boolean array[]=new boolean[26];
-        int lastIndex[]=new int[26];
-        for(int i=0;i<s.length();i++){
-            int index=s.lastIndexOf(s.charAt(i));
-            lastIndex[s.charAt(i)-'a']=index;
+       boolean array[]=new boolean[26];
+       int last[]=new int[26];
+
+       for(int i=0;i<s.length();i++){
+         char ch=s.charAt(i);
+         int index=s.lastIndexOf(ch);
+         last[ch-'a']=index;
+       } 
+
+       Stack<Character>stack=new Stack<>();
+       for(int i=0;i<s.length();i++)
+       {
+
+        if(array[s.charAt(i)-'a']==true){
+            continue;
         }
-       Stack<Character> stack=new Stack<>();
-        for(int i=0;i<s.length();i++){
-            if(array[s.charAt(i)-'a']==true){continue;}
-            while(!stack.isEmpty() && stack.peek()>s.charAt(i) && lastIndex[stack.peek()-'a']>i){
-                array[stack.pop()-'a']=false;
-        
-             }
+
+        while(!stack.isEmpty() && stack.peek()>s.charAt(i) && last[stack.peek()-'a']>i){
+            array[stack.pop()-'a']=false;
+        }
             stack.push(s.charAt(i));
             array[s.charAt(i)-'a']=true;
-        }
-        StringBuilder sb=new StringBuilder();
-        while(!stack.isEmpty()){
-            sb.append(stack.pop());
-        }
-        sb.reverse();
-        return sb.toString();
+       }
+
+       StringBuilder str=new StringBuilder();
+       while(!stack.isEmpty()){
+        str.append(stack.pop());
+       }
+
+       return str.reverse().toString();
     }
 }
