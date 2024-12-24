7534d46075cdf08162e6c132f44f653e23a38c09
Saumya Singh <43070615+Saumya1915@users.noreply.github.com>
2024-12-24 14:28:53 +0530

    Time: 0 ms (100%), Space: 41.1 MB (47.67%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -16,8 +16,21 @@ class Solution {
     }
     public int rob(int[] nums) {
         int dp[]=new int[nums.length];
-        Arrays.fill(dp,-1);
-        return solve(nums,nums.length-1,dp);
+        // Arrays.fill(dp,-1);
+        // return solve(nums,nums.length-1,dp);
+
+        dp[0]=nums[0];
+        for(int i=1;i<nums.length;i++){
+
+            int sum=dp[i-1];
+            int sum2=nums[i];
+            if(i>1){
+                sum2+=dp[i-2];
+            }
+            dp[i]= Math.max(sum,sum2);
+
+        }
+        return dp[nums.length-1];
         
     }
 }
