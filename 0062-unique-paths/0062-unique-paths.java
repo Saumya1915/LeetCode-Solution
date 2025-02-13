1fe05bf64754e03e03d6cad24620fb7541f2d2b6
Saumya Singh <43070615+Saumya1915@users.noreply.github.com>
2025-02-13 17:04:04 +0530

    Time: 0 ms (100%), Space: 40.4 MB (75.24%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -20,8 +20,22 @@ class Solution {
     public int uniquePaths(int m, int n) {
         int dp[][]=new int[m][n];
         for(int[] row:dp){
-            Arrays.fill(row,-1);
+            Arrays.fill(row,0);
         }
-        return solve(m-1,n-1,dp);
+        //return solve(m-1,n-1,dp);
+        for(int i=0;i<n;i++){
+            dp[0][i]=1;
+        }
+        for(int i=0;i<m;i++){
+            dp[i][0]=1;
+        }
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
+                int left=dp[i][j-1];
+                int up=dp[i-1][j];
+                dp[i][j]=left+up;
+            }
+        }
+        return dp[m-1][n-1];
     }
 }
