9b7bcb829f27a08a6ae5a0739fb642ac627de831
Saumya Singh <43070615+Saumya1915@users.noreply.github.com>
2025-03-25 16:23:40 +0530

    Time: 63 ms (32.4%), Space: 55.2 MB (5.42%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.java
+++ 0279-perfect-squares/0279-perfect-squares.java
@@ -34,14 +34,39 @@ class Solution {
        int num=1;
        int dp[][]=new int[array.length][n+1];
        for(int row[]:dp){
-       Arrays.fill(row,-1);
+       Arrays.fill(row,0);
        }
        for(int i=0;i<array.length;i++){
             array[i]=num*num;
             //System.out.println(array[i]);
             num++;
         }
-        return solve(array,array.length-1,n,dp);
+        //return solve(array,array.length-1,n,dp);
+        for(int i=0;i<=n;i++){
+            dp[0][i]=i;
+        }
+
+        for(int i=0;i<dp.length;i++){
+            dp[i][0]=0;
+        }
+
+        for(int i=1;i<dp.length;i++){
+
+            for(int j=1;j<dp[0].length;j++){
+
+                int not=dp[i-1][j];
+                int take=Integer.MAX_VALUE;
+                if(j>=array[i]){
+                int value=dp[i][j-array[i]];
+                    if(value!=Integer.MAX_VALUE){
+                        take=1+value;
+                    }
+                }
+                dp[i][j]=Math.min(not,take);
+            }
+        }
+        return dp[dp.length-1][n];
+
 
 
     }
