96cd312018c934f473d6ec014c3e704c6ff49ec7
Saumya Singh <43070615+Saumya1915@users.noreply.github.com>
2025-06-09 16:06:54 +0530

    Time: 12 ms (66.67%), Space: 45.4 MB (64.23%) - LeetHub

--- 2808-painting-the-walls/2808-painting-the-walls.java
+++ 2808-painting-the-walls/2808-painting-the-walls.java
@@ -29,9 +29,32 @@ class Solution {
     public int paintWalls(int[] cost, int[] time) {
         int dp[][]=new int[cost.length][cost.length+1];
         for(int row[]:dp){
-            Arrays.fill(row,-1);
+            Arrays.fill(row,Integer.MAX_VALUE/2);
         }
-        return solve(cost.length,cost.length-1,cost,time,dp);
+        //return solve(cost.length,cost.length-1,cost,time,dp);
+        for(int i=1;i<=cost.length;i++){
+             if(i<=1+time[0]){
+                dp[0][i]=cost[0];
+             }
+        }
+        for(int i=0;i<cost.length;i++){
+            dp[i][0]=0;
+        }
+        for(int i=1;i<cost.length;i++)
+        {
+
+            for(int j=1;j<=cost.length;j++){
+
+                int not=dp[i-1][j];
+
+                //if(j-1-time[i]>=0)
+                int take=cost[i]+dp[i-1][Math.max(0,j-1-time[i])];
+                
+        
+                dp[i][j]=Math.min(not,take);
+            }
+        }
+        return dp[cost.length-1][cost.length];
         
     }
 }
