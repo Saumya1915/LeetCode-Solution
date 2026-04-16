808463f0d5ee9fe5b994c594116aa1a26fbac22f
Saumya Singh <43070615+Saumya1915@users.noreply.github.com>
2026-04-16 15:40:57 +0530

    Time: 8 ms (15.3%), Space: 45.3 MB (56.63%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.java
+++ 0040-combination-sum-ii/0040-combination-sum-ii.java
@@ -1,35 +1,41 @@
 class Solution {
-    int sum=0;
-    public void solve(int[] candidates, List<List<Integer>> ans,int target,int index,List<Integer> list) {
-         if(sum==target){
-            ans.add(new ArrayList<>(list));
+    public void solve(int index,List<Integer>ans,List<List<Integer>>list,
+    int[] candidates,int target,int sum){
+
+        if(sum==target){
+            list.add(new ArrayList<>(ans));
             return;
         }
-        if(sum>target || index==candidates.length){
+
+        if(sum>target || index>=candidates.length){
             return;
         }
-       
-        list.add(candidates[index]);
-        //System.out.println(list);
-        sum+=candidates[index];
-        solve(candidates,ans,target,index+1,list);
-        sum-=candidates[index];
-        list.remove(list.size()-1);
-        //System.out.println(list);
-        if(index<candidates.length-1 && candidates[index]==candidates[index+1]){
-                while(index<candidates.length-1 && candidates[index]==candidates[index+1]){
-                    index++;
-                }
-        }
-        solve(candidates,ans,target,index+1,list);
 
         
+
+        //take
+        ans.add(candidates[index]);
+        int newSum=sum+candidates[index];
+        solve(index+1,ans,list,candidates,target,newSum);
+        
+        ans.remove(ans.size()-1);
+
+        while (index+1  < candidates.length && candidates[index] == candidates[index + 1]) {
+            index++;
+        }
+
+        //not take
+        solve(index+1,ans,list,candidates,target,sum);
+
+        
+
     }
     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        List<List<Integer>>list=new ArrayList<>();
+        List<Integer>ans=new ArrayList<>();
         Arrays.sort(candidates);
-         List<List<Integer>> ans=new ArrayList<>();
-        List<Integer> list=new ArrayList<>();
-        solve(candidates,ans,target,0,list);
-        return ans;
+        solve(0,ans,list,candidates,target,0);
+        return list;
+        
     }
 }
