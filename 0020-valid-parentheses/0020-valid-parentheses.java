db274c99bd6d32cdb16767348258309e2679a861
Saumya Singh <43070615+Saumya1915@users.noreply.github.com>
2026-05-08 18:36:36 +0530

    Time: 9 ms (5.72%), Space: 44.5 MB (5.61%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean isValid(String s) {
+       Stack<Character>stack=new Stack<>();
+       for(int i=0;i<s.length();i++)
+       {
+         
+            if(!stack.isEmpty() && s.charAt(i)==')' && stack.peek()=='('){
+                stack.pop();
+            }
+            else if(!stack.isEmpty() && s.charAt(i)==']' && stack.peek()=='['){
+                stack.pop();
+            }
+            else if(!stack.isEmpty() && s.charAt(i)=='}' && stack.peek()=='{'){
+                stack.pop();
+            }
+         else
+         {
+            stack.push(s.charAt(i));
+         }
+       }
+       System.out.println(stack);
+       return stack.isEmpty(); 
+    }
+}
