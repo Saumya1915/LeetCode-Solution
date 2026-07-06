dec45e48ce5ad67c7f58c97b9f8a673cc40d0c4c
Saumya Singh <43070615+Saumya1915@users.noreply.github.com>
2026-07-06 17:57:46 +0530

    Time: 0 ms (100%), Space: 41.9 MB (84.96%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,22 +1,34 @@
 class Solution {
-    public int solve(int n,int array[]){
+    // public int solve(int n,int array[]){
+    //     if(n==0){
+    //         return 0;
+    //     }
+    //     if(n==1){
+    //         return 1;
+    //     }
+    //     if(array[n]!=-1){
+    //         return array[n];
+    //     }
+    //     int ans=solve(n-1,array)+solve(n-2,array);
+    //     array[n]=ans;
+    //     return ans;
+    // }
+    public int fib(int n) {
+        int prev_prev=0;
+        int prev=1;
         if(n==0){
-            return 0;
+            return prev_prev;
         }
         if(n==1){
-            return 1;
+            return prev;
         }
-        if(array[n]!=-1){
-            return array[n];
+        for(int i=2;i<=n;i++){
+            int ans=prev_prev+prev;
+            prev_prev=prev;
+            prev=ans;
         }
-        int ans=solve(n-1,array)+solve(n-2,array);
-        array[n]=ans;
-        return ans;
-    }
-    public int fib(int n) {
-        int array[]=new int[n+1];
-        Arrays.fill(array,-1);
-        return solve(n,array);
+        return prev;
+        
         
     }
 }
