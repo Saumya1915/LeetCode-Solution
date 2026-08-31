4cd689e12211320b3f19755e75813593a6223565
Saumya Singh <43070615+Saumya1915@users.noreply.github.com>
2026-08-31 14:55:12 +0530

    Time: 166 ms (5.03%), Space: 126.7 MB (5.01%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -1,71 +1,40 @@
 class Solution {
-    public boolean solve(int target,int[] nums,int index,int dp[][]){
-        if(target==0){
-            return true;
-        }
-        if(index==0){
-            return target==nums[index];
-        }
-        if(dp[index][target]!=-1){
-            return dp[index][target]==1;
-        }
-          
-
-        //notake
-        boolean notake=solve(target,nums,index-1,dp);
-
-        //take
-        boolean take=false;
-        if(nums[index]<=target)
-        take=solve(target-nums[index],nums,index-1,dp);
-
-         dp[index][target]=(take||notake)?1:0;
-         return take||notake;
-    }
-    public boolean canPartition(int[] nums) {
-        int target=0;
-        for(int i=0;i<nums.length;i++){
-            target+=nums[i];
-        }
-
-        if(target%2!=0){
+    public boolean solve(int[] nums, int sum, int index,int ans,int array[][]){
+        if(index>=nums.length){
             return false;
         }
-        //boolean dp[][]=new boolean[nums.length][target+1];
-        //for(int[] row:dp){
-            //Arrays.fill(row,0);
-        //}
-        target=target/2;
-        //return solve(target/2,nums,nums.length-1,dp);
-        
-        boolean prev[]=new boolean[target+1];
-        if(nums[0]<=target)
-         prev[nums[0]]=true;
-
-        for(int i=1;i<nums.length;i++){
-            boolean current[]=new boolean[target+1];
-            current[0]=true;
-            for(int j=1;j<=target;j++){
-
-                boolean notake=prev[j];
-
-                 //take
-                boolean take=false;
-                if(nums[i]<=j)
-                take=prev[j-nums[i]];
-
-                current[j]=(take||notake);
-
-
-            }
-            prev=current;
+        if(ans==sum){
+            return true;
         }
-        return prev[target]==true;
 
+        if(array[index][sum]!=-1){
+            return array[index][sum]==1?true:false;
+        }
+
+        //include
+        boolean a=solve(nums,sum-nums[index],index+1,ans+nums[index],array);
+
+        //don't include
+        boolean b=solve(nums,sum,index+1,ans,array);
+        array[index][sum]=a||b==true?1:0;
+        return a||b;
+
+    }
+    public boolean canPartition(int[] nums) {
+
+        int sum=0;
         
-
-
-
         
+        for(int i=0;i<nums.length;i++){
+            sum+=nums[i];
+        } 
+        int array[][]=new int[nums.length][sum+1];
+        for(int i=0;i<nums.length;i++){
+            Arrays.fill(array[i],-1);
+        }
+       if(sum%2!=0){
+          return false;
+       }
+       return solve(nums,sum,0,0,array);
     }
 }
