e14b0b63daabf26eccca34ebbf9c1ef6567343a5
Saumya Singh <43070615+Saumya1915@users.noreply.github.com>
2026-09-13 17:26:34 +0530

    Time: 5 ms (31.16%), Space: 43.1 MB (56.33%) - LeetHub

---
+++ 1049-last-stone-weight-ii/1049-last-stone-weight-ii.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int lastStoneWeightII(int[] stones) {
+
+        int total=0;
+        for(int i=0;i<stones.length;i++){
+            total+=stones[i];
+        }
+        boolean array[][]=new boolean[stones.length][total+1];
+        array[0][stones[0]]=true;
+        // for(int i=0;i<stones.length;i++){
+        //     array[0][i]=true;
+        // }
+        for(int i=1;i<stones.length;i++){
+            for(int j=0;j<=total;j++){
+                 
+               boolean not=array[i-1][j];
+               boolean take=false;
+               if(stones[i]<=j){
+                    take=array[i-1][j-stones[i]];
+               } 
+               array[i][j]=not||take; 
+
+            }
+        }
+        int diff=Integer.MAX_VALUE;
+        for(int i=0;i<=total;i++){
+            if(array[array.length-1][i]==true)
+            diff=Math.min(diff, Math.abs(total-2*i));
+            
+        }
+        return diff;
+
+        
+    }
+}
